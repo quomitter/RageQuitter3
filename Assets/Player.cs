bfa3dfbc5b00d818fb9b7e2dd061279298dc478e
quomitter <109629602+quomitter@users.noreply.github.com>
2022-08-30 08:30:26 -0400

    changes

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,17 +7,22 @@ public class Player : MonoBehaviour
     Rigidbody2D rb;
     SpriteRenderer rbSprite;
 
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask whatIsGround; 
+
     [SerializeField] float jumpForce;
     [SerializeField] float moveSpeed;
     [SerializeField] float buttonTime = 0.3f;
     [SerializeField] float jumpAmount = 20;
     [SerializeField] float jumpTime;
     [SerializeField] bool jumping;
+    [SerializeField] bool isGrounded; 
 
 
     // Start is called before the first frame update
     void Start()
     {
+        isGrounded = false;
         jumping = false;
         rb = GetComponent<Rigidbody2D>();
         rbSprite = GetComponent<SpriteRenderer>(); 
@@ -34,7 +39,7 @@ public class Player : MonoBehaviour
             jumpTime = 0; 
             
         }
-        if (jumping)
+        if (jumping && isGrounded)
         {
             rb.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
             jumpTime += Time.deltaTime;
@@ -55,5 +60,6 @@ public class Player : MonoBehaviour
         }
 
         Camera.main.transform.position = rb.transform.position + new Vector3(0,0,-10);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, whatIsGround);
     }
 }
